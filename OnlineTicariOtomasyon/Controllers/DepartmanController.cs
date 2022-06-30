48cfb61ab06fedd4ff9a18ab7aafe7bddefbdb82
firat2352 <firatcelik52@gmail.com>
2022-06-30 21:19:05 +0300

    Departman Controller ,DepartmanEkle Actions and regarded views added and also Departman class edited

---
+++ OnlineTicariOtomasyon/Controllers/DepartmanController.cs
@@ -0,0 +1,33 @@
+﻿using OnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        Context _context = new Context();
+        public ActionResult Index()
+        {
+            var departmanlar = _context.Departmans.ToList();
+            return View(departmanlar);
+        }
+
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman departman)
+        {
+            _context.Departmans.Add(departman);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
