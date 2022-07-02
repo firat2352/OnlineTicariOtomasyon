e0ba548ebd7aaf14fa4c5579420f9857a2c75607
firat2352 <firatcelik52@gmail.com>
2022-07-02 13:47:04 +0300

    Cari Controller ,CariEkle actions and view added

---
+++ OnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,32 @@
+﻿using OnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        Context _context = new Context();
+        public ActionResult Index()
+        {
+            return View(_context.Caris.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult CariEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariEkle(Cari cari)
+        {
+            _context.Caris.Add(cari);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
