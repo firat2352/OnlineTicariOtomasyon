a464e660bd8c3ddf2ec5f493459e4b83d111b4bd
firat2352 <firatcelik52@gmail.com>
2022-07-04 09:37:13 +0300

    SatisGetir and SatisGuncelle actions and related view added

--- OnlineTicariOtomasyon/Controllers/SatisController.cs
+++ OnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -53,5 +53,51 @@ namespace OnlineTicariOtomasyon.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult SatisGetir(int id)
+        {
+            List<SelectListItem> urunler = (from urun in _context.Uruns.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = urun.UrunAdi,
+                                                Value = urun.UrunId.ToString(),
+                                            }).ToList();
+
+            List<SelectListItem> cariler = (from cari in _context.Caris.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = cari.CariAd + " " + cari.CariSoyad,
+                                                Value = cari.CariId.ToString(),
+                                            }).ToList();
+
+            List<SelectListItem> personeller = (from personel in _context.Personels.ToList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = personel.PersonelAd + " " + personel.PersonelSoyad,
+                                                    Value = personel.PersonelId.ToString(),
+                                                }).ToList();
+            ViewBag.products = urunler;
+            ViewBag.companies = cariler;
+            ViewBag.personels = personeller;
+
+            return View(_context.SatisHarekets.Find(id));
+        }
+
+        public ActionResult SatisGuncelle(SatisHareket satisHareket)
+        {
+            var satisFind=_context.SatisHarekets.Find(satisHareket.SatisId);
+
+            satisFind.CariId = satisHareket.CariId;
+            satisFind.Adet = satisHareket.Adet;
+            satisFind.Fiyat = satisHareket.Fiyat;
+            satisFind.PersonelId = satisHareket.PersonelId;
+            satisFind.Tarih = satisHareket.Tarih;
+            satisFind.ToplamTutar = satisHareket.ToplamTutar;
+            satisFind.UrunId = satisHareket.UrunId;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
