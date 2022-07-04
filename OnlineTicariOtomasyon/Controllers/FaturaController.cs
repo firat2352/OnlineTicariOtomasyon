0edb59a7172014d94aebb88ec52b12fbbda8ca2d
firat2352 <firatcelik52@gmail.com>
2022-07-04 16:21:17 +0300

    FaturaEkle actions and  relevant view added also Index.cshtml edited

--- OnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ OnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -14,5 +14,19 @@ namespace OnlineTicariOtomasyon.Controllers
         {
             return View(_context.Faturas.ToList());
         }
+
+        [HttpGet]
+        public ActionResult FaturaEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FaturaEkle(Fatura fatura)
+        {
+            var faturaFind=_context.Faturas.Add(fatura);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
