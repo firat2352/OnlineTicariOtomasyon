7c4e47aa025e78e326784e9cbae40d25c619b372
firat2352 <firatcelik52@gmail.com>
2022-07-04 14:53:40 +0300

    FaturaController and Index action added and AdminLayout edited

---
+++ OnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,18 @@
+﻿using OnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        Context _context = new Context();
+        public ActionResult Index()
+        {
+            return View(_context.Faturas.ToList());
+        }
+    }
+}
