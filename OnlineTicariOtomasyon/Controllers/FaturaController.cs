b02ac1b5286e18b51a34a9edb158637b90cf25cd
firat2352 <firatcelik52@gmail.com>
2022-07-04 17:24:36 +0300

    FaturaGetir and FaturGuncelle actions added

--- OnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ OnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -28,5 +28,27 @@ namespace OnlineTicariOtomasyon.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult FaturaGetir(int id)
+        {
+            var findFatura=_context.Faturas.Find(id);
+            return View(findFatura);
+        }
+
+        public ActionResult FaturaGuncelle(Fatura fatura)
+        {
+            var faturaFind=_context.Faturas.Find(fatura.FaturaId);
+            faturaFind.FaturaSeriNo = fatura.FaturaSeriNo;
+            faturaFind.FaturaSiraNo = fatura.FaturaSiraNo;
+            faturaFind.Saat = fatura.Saat;
+            faturaFind.Tarih = fatura.Tarih;
+            faturaFind.TeslimAlan = fatura.TeslimAlan;
+            faturaFind.TeslimEden = fatura.TeslimEden;
+            faturaFind.VergiDairesi = fatura.VergiDairesi;
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
