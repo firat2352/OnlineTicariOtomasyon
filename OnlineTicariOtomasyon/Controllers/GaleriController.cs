c53f08a65095961f8abe627d2c5994ff58e799b2
firat2352 <firatcelik52@gmail.com>
2022-07-05 13:37:49 +0300

    Thumber file ,GaleriController and Index view added and edited

---
+++ OnlineTicariOtomasyon/Controllers/GaleriController.cs
@@ -0,0 +1,18 @@
+﻿using OnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        Context _context = new Context();
+        public ActionResult Index()
+        {
+            return View(_context.Uruns.ToList());
+        }
+    }
+}
