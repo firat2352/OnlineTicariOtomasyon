1c039898ad81dd16868799db8d6e0cc9a09ca2ec
firat2352 <firatcelik52@gmail.com>
2022-07-06 15:01:47 +0300

    ToDo class added and DbSet added about this class

---
+++ OnlineTicariOtomasyon/Models/Siniflar/ToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Siniflar
+{
+    public class ToDo
+    {
+        [Key]
+        public int ToDoId { get; set; }
+
+        [Column(TypeName = "Varchar"), StringLength(30)]
+        public string Title { get; set; }
+
+        [Column(TypeName = "bit")]
+        public bool State { get; set; }
+    }
+}
