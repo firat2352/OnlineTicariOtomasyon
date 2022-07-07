a2590a22cabef65f06f6330e16cbcdf8ca6d22ec
firat2352 <firatcelik52@gmail.com>
2022-07-07 14:19:16 +0300

    AdminLogin actions added (LoginController) and Index view edited

--- OnlineTicariOtomasyon/Controllers/LoginController.cs
+++ OnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -51,5 +51,27 @@ namespace OnlineTicariOtomasyon.Controllers
                 return RedirectToAction("Index", "Login");
             }
         }
+
+        [HttpGet]
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdminLogin(Admin admin)
+        {
+            var result = _context.Admins.FirstOrDefault(x => x.KullaniciAdi == admin.KullaniciAdi && x.Sifre == admin.Sifre);
+            if (result != null)
+            {
+                FormsAuthentication.SetAuthCookie(result.KullaniciAdi, false);
+                Session["KullaniciAd"] = result.KullaniciAdi.ToString();
+                return RedirectToAction("Index", "Kategori");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
     }
 }
